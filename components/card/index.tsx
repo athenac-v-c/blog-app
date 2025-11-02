e04e6376be5f8ab9d05f27afb6c38e58de921965
athenac-v-c <aprilyang065@gmail.com>
2025-11-02 17:45:09 -0500

    feat(card): implement posts section with image container, text container, and styling for blog feed

--- components/card/index.tsx
+++ components/card/index.tsx
@@ -1,40 +1,44 @@
+'use client'
+import Link from 'next/link'
 import './index.css'
 import Image from 'next/image'
-
-interface Post{
+interface CardProps{
     image:string,
     createdAt:Date,
+    category:string,
     title:string,
+
     content:string
 }
-export default function Card(){
+
+export default function Card({image,createdAt,category,title,content}:CardProps){
 
 
+    
 
     return(
-
-        <div className="card-container">
+        
+        
+        <div  className="card-container">
             <div className="image-container">
-                        <Image src="/view.webp" alt = " " fill  />
-                    </div>
-                    <div className="text-container">
-                        <div className="detail">
-                             <span className='date'></span>
-                             <span className='category'></span>
-                        </div>
-                    </div>
-                    <h1 className='title'></h1>
-                    <p className='text-content'></p>
-
-
-
-        
-        
+                <Image src={image} alt = " " fill className='card-img'/>
+            </div>
+            <div className="text-container">
+                <div className="text-detail">
+                    <span className='date'>{createdAt.toDateString()}</span>
+                    -
+                    <span className='text-category'>{category}</span>
+                </div>
+                <h1 className='text-title'><b>{title}</b></h1>
+                <p className='text-content'>{content}</p>
+                <Link href="#" className='text-btn'>Read more</Link>
+            </div>
         </div>
+        )
 
 
 
-    )
+    
 
 
 
--- components/cardList/index.tsx
+++ components/cardList/index.tsx
@@ -1,18 +1,71 @@
 'use client'
+import Card from "../card"
 import Pagination from "../pagination"
 import "./index.css"
-import Image from "next/image"
+
+
+interface Post{
+    image:string,
+    createdAt:Date,
+    category:string,
+    title:string,
+
+    content:string
+}
+
 export default function CardList(){
+
+    const test_posts=[
+        {
+            image:"/view.webp",
+            createdAt:new Date("2025-2-18"),
+            title:"netherlands view",
+            category:"travel",
+            content:"A soft morning mist lingers over the endless rows \
+            of tulip fields, each stripe bursting with vibrant reds, yellows, and \
+            purples beneath a pale blue sky. In the distance, old windmills turn slowly \
+            by the canals, their reflections shimmering in the still water. Bicycles glide \
+            past cobblestone streets lined with gabled houses, and the air carries a calm \
+            rhythm — a quiet harmony of nature and human design that feels timeless and peaceful.purples beneath a pale blue sky. In the distance, old windmills turn slowly \
+            by the canals, their reflections shimmering in the still water. Bicycles glide \
+            past cobblestone streets lined with gabled houses, and the air carries a calm \
+            rhythm — a quiet harmony of nature and human design that feels timeless and peaceful."
+        },
+
+        {
+            image:"/foodView.webp",
+            createdAt:new Date("2025-4-18"),
+            category:"food",
+            title:"my food view",
+            content:"Golden fries, melting cheese, and syrup-filled waffles — the Netherlands’ \
+            comfort food captures the joy of simple moments and cozy flavors"
+        }
+
+    ]
+    
     return (
       
          <div className="cardList-container">
-            <h1>Recent Posts</h1>
+            <h1 className = "title">Recent Posts</h1>
             <div className="posts">
-                <div className="post">
-                    
+
+                {/*<div className="post">*/}
+                {test_posts.map((post:Post)=>(
+                     <Card key={post.image}
+                     image={post.image} 
+                     createdAt={post.createdAt} 
+                     category={post.category} 
+                     title={post.title}
+                     content={post.content}
+                     />
+
+                ))}
+                   
+
+                {/*</div>*/}
 
 
-                </div>
+               
 
 
 
