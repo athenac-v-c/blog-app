36d439c9183b3268981d997a3213c8099e55379e
athenac-v-c <aprilyang065@gmail.com>
2025-11-03 22:48:45 -0500

    feat(menu): add new features to menu component

--- components/menu/index.tsx
+++ components/menu/index.tsx
@@ -1,9 +1,37 @@
 'use client'
+import Link from "next/link"
 import "./index.css"
-
+import Image from "next/image"
 export default function Menu(){
 
     return (
-        <div className="menu-container">Menu</div>
+        <div className="menu-container">
+            <h2 className="menu-subtitle">{"What's hot"}</h2>
+            <h1 className="menu-title">Most Popular</h1>
+            <div className="items">
+                <Link href="\" className="menu-item">
+                    <div className="item-image-container">
+                        <Image src="/style.jpg" alt = "" className="item-img" fill/>
+                    </div>
+                    <div className="item-text-container">
+                        <span>Style</span>
+                        <h3 className="item-postTitle">Style is the expression of individuality through </h3>
+                    
+                        <div className="item-detail">
+                            <span className="item-username">Athena L</span>
+                            <span className="item-date"> - 10.03.2025</span>
+                        </div>
+                    </div>
+                </Link>
+
+            </div>
+            
+
+
+        
+        
+        
+        
+        </div>
     )
 }
